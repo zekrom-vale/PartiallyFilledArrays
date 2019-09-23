90a35d8448b482244ab3a4151eada4a3803856a2
zekrom-vale <sgraven66@gmail.com>
2019-09-22 19:21:26 -0500

    Updated class to be cleaner

--- src/oddEvenSort/ArrayOddEven.java
+++ src/oddEvenSort/ArrayOddEven.java
@@ -2,37 +2,58 @@ package oddEvenSort;
 
 class ArrayOddEven
 {
-	private final long[] a;                 // ref to array a
-	private int nElems;               // number of data items
+	/**
+	 * The reference to the long array
+	 */
+	private final long[] arr;	//Final allows writing to it but not replacing an existing value
+	/**
+	 * The current size of the array
+	 */
+	private int size;
 
 
-	public ArrayOddEven(final int max)          // constructor
-	{
-		this.a = new long[max];                 // create the array
-		this.nElems = 0;                        // no items yet
+	/**
+	 * Alternate constructor
+	 *
+	 * @param max
+	 *                The maximum capacity of the array
+	 */
+	public ArrayOddEven(final int max){
+		this.arr=new long[max];                 // create the array
+		this.size=0;                        // no items yet
 	}
-	public void display()             // displays array contents
-	{
-		for(int j=0; j<this.nElems; j++)       // for each element,
-			System.out.print(this.a[j] + " ");  // display it
+	/**
+	 * Prints the array to the console
+	 */
+	public void display(){
+		//For each element print it to the console including an extra space at the end
+		for(int j=0; j<this.size; j++) System.out.print(this.arr[j]+" ");
 		System.out.println("");
 	}
-	public void insert(final long value)    // put element into array
-	{
-		this.a[this.nElems] = value;             // insert it
-		this.nElems++;                      // increment size
+	/**
+	 * Inserts the given value into the array
+	 *
+	 * @param value
+	 *                  The value to insert
+	 */
+	public void insert(final long value){
+		this.arr[this.size]=value;             // insert it
+		this.size++;                      // increment size
 	}
+	/**
+	 * Sorts the array via Odd Even Sorting, using standard numrical order
+	 */
 	public void oddEvenSort(){
 		boolean done=false;
 		while(!done){
 			done=true;
 			for(int s=0; s<2; s++){
-				for(int i=0; i<this.nElems; i+=2){
-					if(this.a[i+1]>this.a[i+1]){
+				for(int i=0; i<this.size; i+=2){
+					if(this.arr[i]>this.arr[i+1]){
 						done=false;
-						final long temp=this.a[i];
-						this.a[i]=this.a[i+1];
-						this.a[i+1]=temp;
+						final long temp=this.arr[i];
+						this.arr[i]=this.arr[i+1];
+						this.arr[i+1]=temp;
 					}
 				}
 			}
