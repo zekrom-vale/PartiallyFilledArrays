a7b7d2eaa686e0ab7edc1c3ea913673e6d946b53
zekrom-vale <sgraven66@gmail.com>
2019-09-22 19:46:06 -0500

    Updated insetionSort class

--- src/insertionSort/ArrayIns.java
+++ src/insertionSort/ArrayIns.java
@@ -2,50 +2,80 @@ package insertionSort;
 
 class ArrayIns
 {
-	private final long[] a;                 // ref to array a
-	private int size;               // number of data items
-	public ArrayIns(final int max)          // constructor
-	{
-		this.a = new long[max];                 // create the array
+	/**
+	 * The reference to the long array
+	 */
+	private final long[] arr;
+	/**
+	 * The current size of the array, not the capacity
+	 *
+	 * @see #arr.length
+	 */
+	private int size;
+
+	/**
+	 * Alternate constructor
+	 *
+	 * @param max
+	 *                The capacity of the array
+	 */
+	public ArrayIns(final int max){
+		this.arr = new long[max];                 // create the array
 		this.size=0;                        // no items yet
 	}
-	public void display()             // displays array contents
-	{
-		for(int j=0; j<this.size; j++)       // for each element,
-			System.out.print(this.a[j] + " ");  // display it
+	/**
+	 * Prints the array to the console
+	 */
+	public void display(){
+		//For each element in the array print it to the console
+		//Deliminated by a space and a trailing space
+		for(int j=0; j<this.size; j++) System.out.print(this.arr[j]+" ");
 		System.out.println("");
 	}
-	public void insert(final long value)    // put element into array
-	{
-		this.a[this.size]=value;             // insert it
+	/**
+	 * Insert the given element to the array
+	 *
+	 * @param value
+	 *                  he value to insert
+	 */
+	public void insert(final long value){
+		this.arr[this.size]=value;             // insert it
 		this.size++;                      // increment size
 	}
-	public void insertionSort()
-	{
+	/**
+	 * Sort the array via Insertion Sorting by standard numical order
+	 */
+	public void insertionSort(){
 		int in, out;
 
 		for(out=1; out<this.size; out++)     // out is dividing line
 		{
-			final long temp = this.a[out];            // remove marked item
+			final long temp = this.arr[out];            // remove marked item
 			in = out;                      // start shifts at out
-			while(in>0 && this.a[in-1] >= temp) // until one is smaller,
+			while(in>0 && this.arr[in-1] >= temp) // until one is smaller,
 			{
-				this.a[in] = this.a[in-1];            // shift item to right
+				this.arr[in] = this.arr[in-1];            // shift item to right
 				--in;                       // go left one position
 			}
-			this.a[in] = temp;                  // insert marked item
+			this.arr[in] = temp;                  // insert marked item
 		}  // end for
 	}  // end insertionSort()
 
+	/**
+	 * Finds the median value of the array<br>
+	 * <b>Sorts the array</b>
+	 *
+	 * @return The median value of the array
+	 */
 	public long median(){
 		//Sort the array to be able to find the median
 		this.insertionSort();
 		final int at=this.size/2;
 		//If even
 		if(this.size%2==0){
-			return (this.a[at]+this.a[at+1])/2L;
+			return (this.arr[at]+this.arr[at+1])/2L;
 		}
 		//If odd
-		return this.a[at];
+		return this.arr[at];
 	}
-}  // end class ArrayIns
+}
